8423ee9fc51ae2d27c450833918515fdc993d954
Ruhi Singh <ruhis@adobe.com>
2026-09-24 21:40:35 +0530

    Hotfix - Revert Prod Ethos endpoint switch

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -783,7 +783,7 @@ export function getConfig() {
       hlxPreview: /^([a-z0-9-]+)--exlm-prod--adobe-experience-league.(hlx|aem).page$/,
       hlxLive: /^([a-z0-9-]+)--exlm-prod--adobe-experience-league.(hlx|aem).live$/,
       community: 'experienceleaguecommunities.adobe.com',
-      ethos: 'experienceleague-api.ethos09-prod-va7.ethos.adobe.net',
+      // ethos: 'experienceleague-api.ethos09-prod-va7.ethos.adobe.net',
     },
     {
       env: 'STAGE',
@@ -792,7 +792,7 @@ export function getConfig() {
       hlxPreview: /^([a-z0-9-]+)--exlm-stage--adobe-experience-league.(hlx|aem).page$/,
       hlxLive: /^([a-z0-9-]+)--exlm-stage--adobe-experience-league.(hlx|aem).live$/,
       community: 'experienceleaguecommunities-beta.adobe.com',
-      ethos: 'experienceleague-api-stage.ethos09-prod-va7.ethos.adobe.net',
+      // ethos: 'experienceleague-api-stage.ethos09-prod-va7.ethos.adobe.net',
     },
     {
       env: 'DEV',
@@ -801,7 +801,7 @@ export function getConfig() {
       hlxPreview: /^([a-z0-9-]+)--exlm--adobe-experience-league.(hlx|aem).page$/,
       hlxLive: /^([a-z0-9-]+)--exlm--adobe-experience-league.(hlx|aem).live$/,
       community: 'experienceleaguecommunities-beta.adobe.com',
-      ethos: 'experienceleague-api-stage.ethos09-prod-va7.ethos.adobe.net',
+      // ethos: 'experienceleague-api-stage.ethos09-prod-va7.ethos.adobe.net',
     },
   ];
 
@@ -860,9 +860,9 @@ export function getConfig() {
   );
   const cdnHost = currentEnv?.cdn || defaultEnv.cdn;
   const communityHost = currentEnv?.community || defaultEnv.community;
-  const ethosHost = currentEnv?.ethos || defaultEnv.ethos;
+  // const ethosHost = currentEnv?.ethos || defaultEnv.ethos;
   const cdnOrigin = `https://${cdnHost}`;
-  const ethosOrigin = `https://${ethosHost}`;
+  // const ethosOrigin = `https://${ethosHost}`;
   const premiumLearningAuthAPI = `${cdnOrigin}/api/v1/web/alm/authentication`;
   const rawLang = document.querySelector('html').lang || 'en';
   const lang = window.location.hostname.includes(communityHost)
@@ -934,7 +934,7 @@ export function getConfig() {
     plPublicCatalogIds,
     plApiBaseUrl: 'https://learningmanager.adobe.com/primeapi/v2',
     adlsUrl: 'https://learning.adobe.com/courses.result.json',
-    industryUrl: `${ethosOrigin}/api/industries?page_size=200&sort=Order&lang=${lang}`,
+    industryUrl: `${cdnOrigin}/api/industries?page_size=200&sort=Order&lang=${lang}`,
     articleUrl: `${cdnOrigin}/api/articles`,
     solutionsUrl: `${cdnOrigin}/api/solutions?page_size=100`,
     pathsUrl: `${cdnOrigin}/api/paths`,
@@ -954,7 +954,7 @@ export function getConfig() {
     communityAccountURL: isProd
       ? `https://experienceleaguecommunities.adobe.com/?lang=${communityLocale}`
       : `https://experienceleaguecommunities-beta.adobe.com/?lang=${communityLocale}`,
-    interestsUrl: `${ethosOrigin}/api/interests?page_size=200&sort=Order`,
+    interestsUrl: `${cdnOrigin}/api/interests?page_size=200&sort=Order`,
     // Param for localized Community Profile URL
     localizedCommunityProfileParam: `?lang=${communityLocale}`,
     // MPC API Base
